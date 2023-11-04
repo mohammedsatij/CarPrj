90c5164787047a8138821aee970fc1403807790b
QuangVuMinh0301 <148019909+QuangVuMinh0301@users.noreply.github.com>
2023-11-04 22:35:53 +0700

    Update Brand.java

--- src/Brand.java
+++ src/Brand.java
@@ -15,14 +15,38 @@ public class Brand {
         this.price = price;
     }
     
-    public String getBrandID () {
+   public String getBrandID() {
         return brandID;
     }
 
-    public String getBrandName () {
+    public void setBrandID(String brandID) {
+        this.brandID = brandID;
+    }
+
+    public String getBrandName() {
         return brandName;
     }
 
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getSoundBrand() {
+        return soundBrand;
+    }
+
+    public void setSoundBrand(String soundBrand) {
+        this.soundBrand = soundBrand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
     public void setUpdatedBrand (String brandName, String soundBrand, double price) {
         this.brandName = brandName;
         this.soundBrand = soundBrand;
