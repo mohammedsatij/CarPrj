f5425ec903ac25151484707cbb26f44e3a8e95f2
QuangVuMinh0301 <148019909+QuangVuMinh0301@users.noreply.github.com>
2023-11-04 23:07:41 +0700

    Update BrandList.java

--- src/BrandList.java
+++ src/BrandList.java
@@ -119,26 +119,46 @@ public class BrandList extends ArrayList <Brand> {
 
     //Update brand_name, sound_brand, price of an existed brand
     public void updateBrand () {
-         String brandID = check.inputNonBlankString("Enter Brand ID to update: ");
-        int pos = SearchBrandByID(brandID);
-        if(pos == -1){
-            System.out.println("Not Found!!!");
-        }
-        else{
-            String id = check.inputToUpdateBrandID("Enter Brand ID: ", list);
-            String brandName = check.inputNonBlankString("Enter Brand Name: ");
-            String soundBrand = check.inputNonBlankString("Enter sound Brand: ");
-            double price = check.inputPrice("Enter price: ", 0, 1e9);
-            
-            Brand find = list.get(pos);
-            find.setBrandID(id);
-            find.setBrandName(brandName);
-            find.setSoundBrand(soundBrand);
-            find.setPrice(price);
-            
-            
-            System.out.println("Update Successfully!!!");
-        }
+        do {
+            System.out.print("Input brand ID: ");
+            brandID = scanner.nextLine();
+            pos = searchID (brandID);
+            if (pos != -1) {
+                break;
+            }
+            System.out.println("Not found !");
+        } while (true);
+        do {
+            System.out.print("Input brand name: ");
+            brandName = scanner.nextLine();
+            if (brandName.equals("") != true) {
+                break;
+            }
+            System.out.println("The brand name must not be null. Try again !");
+        } while (true);
+        do {
+            System.out.print("Input sound brand: ");
+            soundBrand = scanner.nextLine();
+            if (soundBrand.equals("") != true) {
+                break;
+            }
+            System.out.println("The sound brand must not be null. Try again !");
+        } while (true);
+        do {
+            System.out.print("Input price: ");
+            try {
+                price = Double.parseDouble(scanner.nextLine());
+                if (price <= 0) {
+                    System.out.println("The price must not be null. Try again !");
+                    price = 0;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("The price must be a number. Try again !");
+                price = 0;
+            }
+        } while (price == 0);
+        this.get(0).setUpdatedBrand(brandName, soundBrand, price);
+        System.out.println("Brand has updated successfully !");
     }
 
     //Show the list of the brands
